503a1c6fb5c97a50ae82c58643f2045fb4abe0f8
Paweł Piętak <pawel.pietak@thesoftwarehouse.pl>
2019-01-13 16:30:45 +0100

    Add ESLint and reformat code in order to meet its requirements

--- src/extension/jira.js
+++ src/extension/jira.js
@@ -1,86 +1,79 @@
-"use strict";
-
 const JiraClient = require('jira-connector');
-
-module.exports = {
-    checkCredentials: async (host, account, password) => {
-        try {
-            const client = new JiraClient({
-                host: host,
-                basic_auth: {username: account, password: password}
-            });
-            return await client.myself.getMyself().then(() => true).catch(() => false);    
-        } catch (err) {
-            return false;
-        }
-    },
-    
-    initialize: (host, account, password) => {
-        const username = account;
-        const client = new JiraClient({
-            host: host,
-            basic_auth: {username: account, password: password}
-        });
-
-        return {
-            getWorklogs: (project, day) =>
-                findWorklogs(client, project, username, day),
-
-            getSuggestedTaskKeys: (project, day) =>
-                client
-                    .search
-                    .search({jql: `project = ${project} AND assignee was currentUser() ON ${day} AND status was "In progress" ON ${day} ORDER BY updated ASC`})
-                    .then(result => result.issues.map(task => ({key: task.key, name: task.fields.summary})))
-                    .then((tasks) => tasks.map(task => task.key)),
-
-            findTasksWithKeys: (keys) => {
-                if (keys.length === 0) {
-                    return [];
-                }
-                
-                return client
-                    .search
-                    .search({jql: 'key in (' + keys.join(', ') + ')'})
-                    .then(result => result.issues.map(task => { return {key: task.key, name: task.fields.summary}}));
-            },
-
-            sendWorklog: (day, task, hours) =>
-                client
-                    .issue
-                    .addWorkLog({
-                        issueKey: task,
-                        notifyUsers: false,
-                        worklog: {timeSpent: `${hours}h`, started: jiraDateTime(day)}
-                    }),
-        };
-    }
-};
 
 const jiraDateTime = day => new Date(day).toISOString().replace('Z', '+0000');
 const dayStart = day => `${day} 00:00`;
 const dayEnd = day => `${day} 23:59`;
 
-const findWorklogs = async (client, project, username, day) => {
-    return client
+const findWorklogs = async (client, project, username, day) => client
+  .search
+  .search({
+    jql: `project = ${project} AND worklogAuthor = currentUser() AND worklogDate = ${day} ORDER BY updated ASC`,
+    fields: ['worklog'],
+  })
+  .then(res => res.issues.map((task) => {
+    const spentSeconds = task.fields.worklog
+      .worklogs
+      .filter(
+        worklog => (worklog.author.emailAddress === username || worklog.author.name === username)
+          && new Date(worklog.started) >= new Date(dayStart(day))
+          && new Date(worklog.started) <= new Date(dayEnd(day))
+      )
+      .reduce((sum, worklog) => sum + worklog.timeSpentSeconds, 0);
+    return {
+      key: task.key,
+      hours: Math.floor(spentSeconds / 3600),
+    };
+  }));
+
+module.exports = {
+  checkCredentials: async (host, account, password) => {
+    try {
+      const client = new JiraClient({
+        host,
+        basic_auth: { username: account, password },
+      });
+      return await client.myself.getMyself().then(() => true).catch(() => false);
+    } catch (err) {
+      return false;
+    }
+  },
+
+  initialize: (host, account, password) => {
+    const username = account;
+    const client = new JiraClient({
+      host,
+      basic_auth: { username: account, password },
+    });
+
+    return {
+      getWorklogs: (project, day) => findWorklogs(client, project, username, day),
+
+      getSuggestedTaskKeys: (project, day) => client
         .search
-        .search({
-            jql: `project = ${project} AND worklogAuthor = currentUser() AND worklogDate = ${day} ORDER BY updated ASC`,
-            fields: ['worklog']
-        })
-        .then(res => {
-            return res.issues.map(task => {
-                const spentSeconds = task.fields.worklog
-                    .worklogs
-                    .filter(worklog =>
-                        (worklog.author.emailAddress === username || worklog.author.name === username) 
-                        && new Date(worklog.started) >= new Date(dayStart(day))
-                        && new Date(worklog.started) <= new Date(dayEnd(day))
-                    )
-                    .reduce((sum, worklog) => sum + worklog.timeSpentSeconds, 0);
-                return {
-                    key: task.key,
-                    hours: Math.floor(spentSeconds / 3600)
-                };
-            })
-        });
+        .search({ jql: `project = ${project} AND assignee was currentUser() ON ${day} AND status was "In progress" ON ${day} ORDER BY updated ASC` })
+        .then(result => result.issues.map(task => ({ key: task.key, name: task.fields.summary })))
+        .then(tasks => tasks.map(task => task.key)),
+
+      findTasksWithKeys: (keys) => {
+        if (keys.length === 0) {
+          return [];
+        }
+
+        return client
+          .search
+          .search({ jql: `key in (${keys.join(', ')})` })
+          .then(result => result.issues.map(
+            task => ({ key: task.key, name: task.fields.summary })
+          ));
+      },
+
+      sendWorklog: (day, task, hours) => client
+        .issue
+        .addWorkLog({
+          issueKey: task,
+          notifyUsers: false,
+          worklog: { timeSpent: `${hours}h`, started: jiraDateTime(day) },
+        }),
+    };
+  },
 };
--- src/interface/cli/input/prompts.js
+++ src/interface/cli/input/prompts.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const moment = require('moment');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
@@ -8,98 +6,96 @@ const staticSelectPrompt = require('../../../lib/inquirer/staticSelectPrompt');
 inquirer.registerPrompt('static-select', staticSelectPrompt);
 
 module.exports = {
-    askForProject: () =>
-        inquirer
-            .prompt([{
-                name: 'project',
-                type: 'input',
-                message: 'Set project key (like "JRA")',
-            }])
-            .then(({project}) => project),
+  askForProject: () => inquirer
+    .prompt([{
+      name: 'project',
+      type: 'input',
+      message: 'Set project key (like "JRA")',
+    }])
+    .then(({ project }) => project),
 
-    askForHost: () =>
-        inquirer
-            .prompt([{
-                name: 'host',
-                type: 'input',
-                message: 'Set base URL (like "jira.atlassian.net")',
-            }])
-            .then(({host}) => host),
+  askForHost: () => inquirer
+    .prompt([{
+      name: 'host',
+      type: 'input',
+      message: 'Set base URL (like "jira.atlassian.net")',
+    }])
+    .then(({ host }) => host),
 
-    askForAccount: () =>
-        inquirer
-            .prompt([{
-                name: 'account',
-                type: 'input',
-                message: 'Jira login',
-            }])
-            .then(({account}) => account),
+  askForAccount: () => inquirer
+    .prompt([{
+      name: 'account',
+      type: 'input',
+      message: 'Jira login',
+    }])
+    .then(({ account }) => account),
 
-    askForPassword: async () =>
-        inquirer
-            .prompt([{
-                name: 'password',
-                type: 'password',
-                message: 'Jira password',
-            }])
-            .then(({password}) => password),
+  askForPassword: async () => inquirer
+    .prompt([{
+      name: 'password',
+      type: 'password',
+      message: 'Jira password',
+    }])
+    .then(({ password }) => password),
 
-    promptDay: async () =>
-        inquirer
-            .prompt({
-                type: 'static-select',
-                name: 'date',
-                message: 'Choose date (use arrows):',
-                choices: Array
-                    .from({length: 31}, (x, i) => i)
-                    .map(x => moment().subtract(x, 'days'))
-                    .map(date => ({name: chalk.cyan(date.format('YYYY-MM-DD')) + chalk.dim(date.format(' (dddd)')), value: date.format('YYYY-MM-DD')})),
-            })
-            .then(({date}) => date),
+  promptDay: async () => inquirer
+    .prompt({
+      type: 'static-select',
+      name: 'date',
+      message: 'Choose date (use arrows):',
+      choices: Array
+        .from({ length: 31 }, (x, i) => i)
+        .map(x => moment().subtract(x, 'days'))
+        .map(date => ({
+          name: chalk.cyan(date.format('YYYY-MM-DD')) + chalk.dim(date.format(' (dddd)')),
+          value: date.format('YYYY-MM-DD'),
+        })),
+    })
+    .then(({ date }) => date),
 
-    promptTask: async (day, choices) => {
-        if (!Array.isArray(choices)) {
-            choices = Object
-                .keys(choices)
-                .filter(source => (choices[source] || []).length > 0)
-                .map(source => [new inquirer.Separator(source), ...choices[source]])
-                .reduce((acc, val) => acc.concat(val), []);
-        }
-        
-        if (choices.length === 0) {
-            return Promise.resolve(null);
-        }
-        
-        return inquirer
-            .prompt({
-                type: 'list',
-                name: 'task',
-                message: `Choose task:`,
-                choices: choices,
-                filter: task => task.match(/^\w+\-\d+/)[0],
-            })
-            .then(({task}) => task);
-    },
+  promptTask: async (day, choices) => {
+    let output = [];
 
-    promptHours: async (choices, defaultChoice) =>
-        inquirer
-            .prompt({
-                type: 'list',
-                name: 'hours',
-                message: 'Choose time spent:',
-                choices: choices,
-                filter: time => time.match(/\d+/)[0],
-                default: defaultChoice,
-            })
-            .then(({hours}) => hours),
+    if (!Array.isArray(choices)) {
+      output = Object
+        .keys(choices)
+        .filter(source => (choices[source] || []).length > 0)
+        .map(source => [new inquirer.Separator(source), ...choices[source]])
+        .reduce((acc, val) => acc.concat(val), []);
+    }
 
-    promptConfirmation: async (hours, task) =>
-        inquirer
-            .prompt({
-                type: 'confirm',
-                name: 'confirmed',
-                message: `Do you confirm to log ${hours}h of work in task ${task}?`,
-                default: true
-            })
-            .then(({confirmed}) => confirmed),
+    if (output.length === 0) {
+      return Promise.resolve(null);
+    }
+
+    return inquirer
+      .prompt({
+        type: 'list',
+        name: 'task',
+        message: 'Choose task:',
+        choices: output,
+        filter: task => task.match(/^\w+-\d+/)[0],
+      })
+      .then(({ task }) => task);
+  },
+
+  promptHours: async (choices, defaultChoice) => inquirer
+    .prompt({
+      type: 'list',
+      name: 'hours',
+      message: 'Choose time spent:',
+      choices,
+      filter: time => time.match(/\d+/)[0],
+      default: defaultChoice,
+    })
+    .then(({ hours }) => hours),
+
+  promptConfirmation: async (hours, task) => inquirer
+    .prompt({
+      type: 'confirm',
+      name: 'confirmed',
+      message: `Do you confirm to log ${hours}h of work in task ${task}?`,
+      default: true,
+    })
+    .then(({ confirmed }) => confirmed),
 };
